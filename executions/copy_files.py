e4329fddd725c64fb771333d3ff69cd7f645c726
Exelo <exelo.corp@gmail.com>
2022-10-13 20:06:38 +0200

    A

--- executions/copy_files.py
+++ executions/copy_files.py
@@ -8,12 +8,18 @@ def copy_files(d_name):
     if ask_input("Do you want your library ?"):
         os.mkdir(f"{d_name}/lib")
         for lib in os.listdir("./data/lib"):
+            if lib == ".gitkeep":
+                os.remove("./data/lib/.gitkeep")
+                continue
             os.mkdir(f"{d_name}/lib/{lib}")
             for file in os.listdir(f"./data/lib/{lib}"):
                 shutil.copy(f"./data/lib/{lib}/{file}", f"{d_name}/lib/{lib}")
     if ask_input("Do you want your include ?"):
         os.mkdir(f"{d_name}/include")
         for i in os.listdir("./data/include"):
+            if i == ".gitkeep":
+                os.remove("./data/lib/.gitkeep")
+                continue
             shutil.copy(f"./data/include/{i}", f"{d_name}/include")
     if ask_input("Do you want your .gitignore ?"):
         shutil.copy("./data/.gitignore", f"{d_name}")
